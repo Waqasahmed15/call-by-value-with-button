989daa6bb2f5585b264bcba4f9a4b84824bd9b22
unknown <waqasahmedwikki15@gmail.com>
2024-03-05 20:34:09 +0500

    ok

---
+++ app.js
@@ -0,0 +1,6 @@
+function changeBtn(idValue,bgColor) {
+  var btn = document.getElementById(idValue);
+  btn.style.width = "200px";
+  btn.style.height = "40px";
+  btn.style.backgroundColor = bgColor;
+}
